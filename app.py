0103b840eba9c199eb36e7c23b9e4c2fe219b40c
ivanwonghs <ivanwong85@yahoo.com.hk>
2026-02-28 16:56:04 +0800

    Update age classification model and fix header typo

--- app.py
+++ app.py
@@ -3,12 +3,12 @@ from PIL import Image
 import streamlit as st
 
 # Streamlit UI
-st.write("Title: Age Classification using ViT")
+st.heads("Title: Age Classification using ViT")
 
 # Load the age classification pipeline
 # The code below should be placed in the main part of the program
 age_classifier = pipeline("image-classification",
-                          model="nateraw/vit-age-classifier")
+                          model="prithivMLmods/Age-Classification-SigLIP2")
 
 image_name = "middleagedMan.jpg"
 image_name = Image.open(image_name).convert("RGB")
